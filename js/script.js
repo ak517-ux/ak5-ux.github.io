80fbc5232ff06284a6148cc6ce78b70e0db37b18
ak517-ux <ddt3kirill201401@gmail.com>
2021-06-15 07:55:21 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+ let speed = document.getElementById("speed"),
+ 		 res = document.getElementById("res");
+
+res.onclick = function() {
+    document.getElementById("speed").innerHTML = '100km/h';
+}
